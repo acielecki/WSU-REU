03f73c12143dcba243181b942b254be82000bdd7
Alissa Cielecki <55100274+acielecki@users.noreply.github.com>
2020-07-23 09:45:20 -0400

    Update retrv_commit_content.py
    Now takes an account parameter instead of hard-coded credentials.

--- Collecting Commits/retrv_commit_content.py
+++ Collecting Commits/retrv_commit_content.py
@@ -7,11 +7,11 @@ import base64
 import mysql.connector
 
 class RetrvCommitContent():
-    def __init__(self, repo_id):    
+    def __init__(self, repo_id, account):    
         self.repo_id  = repo_id 
         
-        self.username = "*********"
-        self.token = "*****************************"
+        self.username = account['username']
+        self.token = account['token']
         
         self.commit_content = []
         
@@ -25,50 +25,56 @@ class RetrvCommitContent():
     def connect_to_db(self):
         self.mydb = mysql.connector.connect(
           host="localhost",
-          user="*********",
-          password="****************************",
-          database="test"
+          user="root",
+          password="***********",
+          database="test1"
         )
         
     def write_to_db(self):
-        header = str(list(self.commit_content[0].keys()))
-        remove_chars = ['[', ']', '\'']
-        for char in remove_chars:
-            header = header.replace(char, '')
+        if not self.commit_content:
+            self.collect_commit_content()
+        else:
+            header = str(list(self.commit_content[0].keys()))
+            remove_chars = ['[', ']', '\'']
+            for char in remove_chars:
+                header = header.replace(char, '')
+                
+            columns = "sha VARCHAR (255), path VARCHAR(255), type VARCHAR(255), content TEXT"
+            table_stmnt = "CREATE TABLE IF NOT EXISTS %s (%s);" %('content_' + str(self.repo_id), columns)
+            cursor = self.mydb.cursor()
+            cursor.execute(table_stmnt)
             
-        columns = "sha VARCHAR (255), path VARCHAR(255), type VARCHAR(255), content TEXT"
-        table_stmnt = "CREATE TABLE IF NOT EXISTS %s (%s);" %('content_' + str(self.repo_id), columns)
-        cursor = self.mydb.cursor()
-        cursor.execute(table_stmnt)
-        
-        for commit in self.commit_content:
-            row = list(commit.values())
-            insert_stmnt = "INSERT INTO %s (%s) VALUES ('%s', '%s', '%s', '%s');" % ('content_' + str(self.repo_id), header, row[0], row[1], row[2], str(row[3]).replace('\'', ''))
-            #print(insert_stmnt)
-            cursor.execute(insert_stmnt)
-            self.mydb.commit()
+            for commit in self.commit_content:
+                row = list(commit.values())
+                insert_stmnt = "INSERT INTO %s (%s) VALUES ('%s', '%s', '%s', '%s');" % ('content_' + str(self.repo_id), header, row[0], row[1], row[2], str(row[3]).replace('\'', ''))
+                cursor.execute(insert_stmnt)
+                self.mydb.commit()
             
     def parse_commits(self, commit_url):
         print("Retrieving commit content for %s"  %(commit_url))
         result = self.http_get_call(commit_url)
 
-        allitems = result['tree']
-        
+        if 'tree' in result:
+            allitems = result['tree']
+        else:
+            allitems = {}
+            
         for item in allitems:
             if item['type'] == 'tree':
                 self.parse_commits(item['url'])
             else:                             
                 result2 = self.http_get_call(item['url'])
-                content = result2['content']
-                content = base64.b64decode(content)
+                if 'content' in result2:
+                    content = result2['content']
+                    content = base64.b64decode(content)
 
-                record = {}
-                record['sha'] = item['sha']
-                record['path'] = item['path']
-                record['type'] = item['type']
-                record['content'] = content
+                    record = {}
+                    record['sha'] = item['sha']
+                    record['path'] = item['path']
+                    record['type'] = item['type']
+                    record['content'] = content
                 
-                self.commit_content.append(record)
+                    self.commit_content.append(record)
             
     def collect_commit_content(self):
         cursor = self.mydb.cursor()
@@ -85,8 +91,9 @@ class RetrvCommitContent():
 
 #Automate for each repository
 #df = pd.read_csv('Repository_List.csv')
-#ids = df['id']
-#for repo_id in ids:
-    #RCC = RetrvCommitContent(str(repo_id))
+#account = {'username': 'acielecki', 'token': '***********************'}
+
+#for repo_id in df['id']:
+    #RCC = RetrvCommitContent(str(repo_id), account)
     #RCC.connect_to_db()
     #RCC.collect_commit_content()
